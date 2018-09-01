9b1d01f1033cb0b79726c4dff3ebb89c15056521
John Horback <jhorback@gmail.com>
2018-09-01 00:31:29 -0400

    Created the CommandExecutor

--- scripts/webdev.mjs
+++ scripts/webdev.mjs
@@ -22,17 +22,103 @@ import webpackConfig from "./webpack.config.mjs";
  *    --chrome
  */
 
+class CommandExecutor {
+  constructor(defaultEnv) {
+    this.commands = {};
+    this.options = {};
+    this.flags = {};
+    this.env = defaultEnv || {};
+    this.env._ = [];
+  }
 
-const commands = {
-  help: (args) => {
-    // console.log("HELP".green, args);
+  addCommand(name, {help, execute, updateEnv = null}) {
+    const command = {name, updateEnv, execute, help};
+    this.commands[name] = command;
+  }
+
+  addHelpCommand() {
+
+  }
+
+  addOption(name, {
+      help,
+      flag = null,
+      defaultValue = null,
+      updateEnv
+    }) {
+    const option = {flag, name, updateEnv, defaultValue, help};
+    
+    this.options[`--${name}`] = option;
+
+    if (flag) {
+      this.flags[`-${flag}`] = option;
+    }
+
+    if (defaultValue) {
+      this.env[name] = defaultValue;
+    }
+  }
+
+  execute(beforeExecute) {
+    const [,, commandName, ...args] = process.argv;
+    const command = this.commands[commandName];
+
+    if (!command) {
+      console.log(`webdev command ${commandName} is not supported`.red);
+    }
+    
+    command.updateEnv && command.updateEnv(this.env);
+    args.forEach((arg) => {
+      const option = this.options[arg] || this.flags[arg];
+      if (option && option.updateEnv) {
+        option.updateEnv(this.env);
+      } else {
+        this.env._.push(option);
+      }
+    });
+
+    beforeExecute && beforeExecute(this.env);
+    command.execute(this.env);
+  }
+}
+
+
+
+const executor = new CommandExecutor({target: "chrome"});
+executor.addHelpCommand();
+
+executor.addCommand("build", {
+  help: "",
+  execute: (env) => {
+    const compiler = getCompiler(env);
+    compiler.run((err, stats) => {
+      if (err) {
+        throw err
+      }
+      if (env.verbose) {
+        process.stdout.write(stats.toString({
+          colors: true,
+          modules: false,
+          children: false,
+          chunks: false,
+          chunkModules: false
+        }) + '\n\n');
+      }
+    });
+  }
+});
+
+executor.addCommand("serve", {
+  help: "",
+
+  updateEnv: (env) => {
+    env.isDevServer = true;
   },
 
-  serve: (args) => {
+  execute: (env) => {
     console.log("Starting dev server...".green);
 
-    args.isDevServer = true;
-    const config = webpackConfig(args);
+    const config = webpackConfig(env);
     const port = config.devServer.port;
     const url = `http://localhost:${port}/`;
 
@@ -46,53 +132,87 @@ const commands = {
       multiStep : false
     }));
 
-    const compiler = getCompiler(args);
+    const compiler = getCompiler(env);
     const server = new WebpackDevServer(compiler, Object.assign({
-      quiet: !args.verbose,
+      quiet: !env.verbose,
       hot: true
     }, config.devServer));
    
     server.listen(port, "localhost", () => {
       console.log(`Server is listening on: ${url}`.green);
     });
+  }
+})
 
-  },
+executor.addOption("dev", {
+  flag: "d",
+  help: "",
+  updateEnv: (env) => {
+    env.mode = "development";
+  }
+});
 
-  build: (args) => {
-    const compiler = getCompiler(args);
-    compiler.run((err, stats) => {
-      if (err) {
-        throw err
-      }
-      if (args.verbose) {
-        process.stdout.write(stats.toString({
-          colors: true,
-          modules: false,
-          children: false,
-          chunks: false,
-          chunkModules: false
-        }) + '\n\n');
-      }
+executor.addOption("prod", {
+  flag: "p",
+  help: "",
+  updateEnv: (env) => {
+    env.mode = "production";
+  }
+});
+
+executor.addOption("verbose", {
+  flag: "v",
+  help: "",
+  defaultValue: false,
+  updateEnv: (env) => {
+    env.verbose = true;
+  }
+});
+
+executor.addOption("ie", {
+  help: "",
+  updateEnv: (env) => {
+    env.target = "ie";
+  }
+});
+
+executor.addOption("chrome", {
+  help: "",
+  updateEnv: (env) => {
+    env.target = "chrome";
+  }
+});
+
+executor.addOption("modern", {
+  help: "",
+  updateEnv: (env) => {
+    env.target = "modern";
+  }
+});
+
+executor.execute((env) => {
+  const envh = new EnvHelper(env);
+  console.log(envh.description.green);
+  if (env.verbose) {
+    console.log("ENV:");
+    Object.keys(env).sort(alphaNumericSort).forEach((key) => {
+      console.log(`  ${key}: ${env[key]}`);
     });
+    console.log("");
   }
-};
+});
 
-
-runCommand();
-
-
-function runCommand() {
-  const args = parseArgs();
-  const command = commands[args.command];
-  const envh = new EnvHelper(args);
-  if (command) {
-    console.log(envh.description.green);
-
-    command(args);
-  } else {
-    console.log(`webdev command ${args.command} is not supported`.red);
+function alphaNumericSort (a, b) {
+  if (a === b) {
+      return 0;
   }
+  if (typeof a === typeof b) {
+      return a < b ? -1 : 1;
+  }
+  return typeof a < typeof b ? -1 : 1;
 }
+
+
 
 
 function getCompiler(args, config) {
@@ -100,44 +220,6 @@ function getCompiler(args, config) {
   config.plugins.push(new progress());
   const compiler = webpack(config);
   return compiler;
-}
-
-function parseArgs() {
-  const [,,command,...args] = process.argv;
-  let env = {
-    command,
-    verbose: false,
-    _: []
-  };
-
-  return args.reduce((env, arg) => {
-    switch(arg) {
-      case "--dev":
-      case "-d":
-        env.mode = "development";
-        break;
-      case "--prod":
-      case "-p":
-        env.mode = "production";
-        break;
-      case "--ie":
-        env.target = "ie";
-        break;
-      case "--modern":
-        env.target = "modern";
-        break;
-      case "--chrome":
-        env.target = "chrome";
-        break;
-      case "--verbose":
-      case "-v":
-        env.verbose = true;
-        break;
-      default:
-        env._.push(arg);
-    }
-    return env;
-  }, env);
 }
 
 
